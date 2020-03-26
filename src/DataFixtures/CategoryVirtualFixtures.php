345b25b786ec16b605934b17cdfe606edb26741b
Verynicedev <61272094+Verynicedev@users.noreply.github.com>
2020-03-26 11:22:14 +0100

    VR Fixtures "CATEGORY" update

--- src/DataFixtures/CategoryVirtualFixtures.php
+++ src/DataFixtures/CategoryVirtualFixtures.php
@@ -10,17 +10,25 @@ class CategoryVirtualFixtures extends Fixture
 {
     public const CATEGORY_1 = 'category-1';
     public const CATEGORY_2 = 'category-2';
+    public const CATEGORY_6 = 'category-6';
     public function load(ObjectManager $manager)
     {
         $category = new CategoryVirtual();
-        $category->setNom('-18 ans');
+        $category->setNom('Tout public');
         $manager->persist($category);
 
         $category2 = new CategoryVirtual();
-        $category2->setNom('+18 ans');
+        $category2->setNom('Interdit aux -16 ans');
         $manager->persist($category2);
+
+        $category6 = new CategoryVirtual();
+        $category6->setNom('Interdit aux -18 ans');
+        $manager->persist($category6);
+
         $manager->flush();
+        
         $this->addReference(self::CATEGORY_1, $category);
         $this->addReference(self::CATEGORY_2, $category2);
+        $this->addReference(self::CATEGORY_6, $category6);
     }
 }
